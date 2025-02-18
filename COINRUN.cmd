1875e1509b0be3262f2e345f4309cdbe780256ad
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-02-18 11:32:35 +0700

    COINRUN.cmd

--- COINRUN.cmd
+++ COINRUN.cmd
@@ -16,5 +16,5 @@
 :: Smaller pools also often have smaller fees/payout limits.
 
 cd /d "%~dp0"
-xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u XRP:rs2dgzYeqYqsk8bvkQR5YPyqsXYcA24MP2:563177.WORKER_NAME_2 -p x
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u XRP:rNxp4h8apvRis6mJf9Sh8C6iRxfrDWN7AV:435319491.WORKER_NAME_2 -p x
 pause 
